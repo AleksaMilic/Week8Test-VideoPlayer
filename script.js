ba33e19cd064830b4f815e46061bffc96c8124b7
AleksaMilic <aleksa.milic@quantoxtechnology.com>
2021-10-28 09:49:36 +0200

    a

---
+++ script.js
@@ -0,0 +1,84 @@
+const player = document.querySelector(".player");
+const video = player.querySelector(".viewer");
+const progress = player.querySelector(".progress");
+const progressBar = player.querySelector(".filled");
+const toggle = player.querySelector(".toggle");
+const fullscreen = player.querySelector(".fullscreen");
+const skipButtons = player.querySelectorAll("[data-skip]");
+const ranges = player.querySelectorAll(".slider");
+
+// build functions
+function togglePlay() {
+  if (video.paused) {
+    video.play();
+  } else {
+    video.pause();
+  }
+}
+
+function spaceBarTogglePlay(e) {
+  if (e.keyCode == 32) {
+    togglePlay();
+  }
+}
+
+function updateButton() {
+  const icon = this.paused ? "►" : "❚❚";
+  toggle.textContent = icon;
+}
+
+function skip() {
+  video.currentTime += parseFloat(this.dataset.skip);
+}
+
+function handleRangeUpdate() {
+  const sliderValue = this.value;
+  this.setAttribute("title", sliderValue);
+  video[this.name] = sliderValue;
+  console.log(this.event);
+}
+
+function handleProgress() {
+  const percent = (video.currentTime / video.duration) * 100;
+  progressBar.style.flexBasis = `${percent}%`;
+}
+
+function scrub(e) {
+  const scrubTime = (e.offsetX / progress.offsetWidth) * video.duration;
+  video.currentTime = scrubTime;
+}
+
+function toggleFullscreen() {
+  if (!document.fullscreenElement) {
+    player.requestFullscreen();
+    fullscreen.textContent = "↙";
+  } else {
+    if (document.exitFullscreen) {
+      document.exitFullscreen();
+      fullscreen.textContent = "️️️️↔️";
+    }
+  }
+}
+
+// event listeners
+video.addEventListener("click", togglePlay);
+video.addEventListener("play", updateButton);
+video.addEventListener("pause", updateButton);
+video.addEventListener("timeupdate", handleProgress);
+
+fullscreen.addEventListener("click", toggleFullscreen);
+video.addEventListener("dblclick", toggleFullscreen);
+
+toggle.addEventListener("click", togglePlay);
+document.addEventListener("keypress", spaceBarTogglePlay);
+skipButtons.forEach((button) => button.addEventListener("click", skip));
+ranges.forEach((range) => range.addEventListener("change", handleRangeUpdate));
+ranges.forEach((range) =>
+  range.addEventListener("mousemove", handleRangeUpdate)
+);
+
+let mousedown = false;
+progress.addEventListener("click", scrub);
+progress.addEventListener("mousemove", (e) => mousedown && scrub(e));
+progress.addEventListener("mousedown", () => (mousedown = true));
+progress.addEventListener("mouseup", () => (mousedown = false));
